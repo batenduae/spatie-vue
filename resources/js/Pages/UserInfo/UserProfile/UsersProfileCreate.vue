cab9f1aa452fbdbe0d35653acf997726ad13187a
Baten <batenduae@gmail.com>
2024-09-23 14:42:31 +0600

    latest changes 23-sep User Details setup done

---
+++ resources/js/Pages/UserInfo/UserProfile/UsersProfileCreate.vue
@@ -0,0 +1,115 @@
+<script setup>
+import {useForm} from "@inertiajs/vue3";
+import TextInput from "@/Components/TextInput.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import InputError from "@/Components/InputError.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import AdminButton from "@/Components/AdminComponents/Buttons/AdminButton.vue";
+import Card from "@/Components/AdminComponents/Cards/Card.vue";
+import PageHeader from "@/Components/AdminComponents/Heading/PageHeader.vue";
+import {usePermissions} from "@/composables/permissions.js";
+import SpatieAdminLayout from "@/Layouts/SpatieAdminLayout.vue";
+import {onMounted, onUpdated} from "vue";
+
+const {hasPermission, showFlash} = usePermissions();
+onMounted(showFlash)
+onUpdated(showFlash)
+const form = useForm({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+});
+defineOptions({layout: SpatieAdminLayout});
+</script>
+
+<template>
+    <PageHeader text="Create User" title="Users/Create">
+        <AdminButton
+            v-if="hasPermission('user.view')"
+            button-text="Go Back"
+            button-type="backward"
+            route-name="users.index"
+        />
+    </PageHeader>
+    <Card class="mx-auto w-96">
+        <div class="py-4 font-semibold">User Information</div>
+        <form @submit.prevent="form.post(route('users.store'))">
+            <div>
+                <InputLabel for="name" value="Name"/>
+
+                <TextInput
+                    id="name"
+                    v-model="form.name"
+                    autocomplete="name"
+                    autofocus
+                    required
+                    type="text"
+                />
+
+                <InputError :message="form.errors.name" class="mt-2"/>
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="email" value="Email"/>
+
+                <TextInput
+                    id="email"
+                    v-model="form.email"
+                    autocomplete="username"
+                    required
+                    type="email"
+                />
+
+                <InputError :message="form.errors.email" class="mt-2"/>
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="password" value="Password"/>
+
+                <TextInput
+                    id="password"
+                    v-model="form.password"
+                    autocomplete="new-password"
+                    required
+                    type="password"
+                />
+
+                <InputError :message="form.errors.password" class="mt-2"/>
+            </div>
+
+            <div class="mt-4">
+                <InputLabel
+                    for="password_confirmation"
+                    value="Confirm Password"
+                />
+
+                <TextInput
+                    id="password_confirmation"
+                    v-model="form.password_confirmation"
+                    autocomplete="new-password"
+                    required
+                    type="password"
+                />
+
+                <InputError
+                    :message="form.errors.password_confirmation"
+                    class="mt-2"
+                />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <PrimaryButton
+                    v-if="hasPermission('user.create')"
+                    :class="{ 'opacity-25': form.processing }"
+                    :disabled="form.processing"
+                    class="ms-4"
+                >
+                    Create
+                </PrimaryButton>
+            </div>
+        </form>
+    </Card>
+</template>
+
+<style scoped></style>
--- routes/adminRoutes.php
+++ routes/adminRoutes.php
@@ -1,11 +1,16 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserAddressController;
+use App\Http\Controllers\UserEducationalBackgroundController;
+use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\UserSocialController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 
 Route::resource('/posts', PostController::class);
-Route::middleware([])->group(function () {
+Route::resource('/userAddress', UserAddressController::class);
+Route::resource('/userEducation', UserEducationalBackgroundController::class);
+Route::resource('/userProfile', UserProfileController::class);
+Route::resource('/userSocial', UserSocialController::class);
 
-});
