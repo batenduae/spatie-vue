7f4a94de1f11bde403a5102e6a857f6d435e6c2a
Baten <batenduae@gmail.com>
2024-10-04 21:17:54 +0600

    latest changes 04-OCt Permission Re-setup

--- app/SpatieContainer/SpatieMiddleware/SpatiePermissionMiddleware.php
+++ app/SpatieContainer/SpatieMiddleware/SpatiePermissionMiddleware.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection ALL */
 
 namespace App\SpatieContainer\SpatieMiddleware;
 
@@ -16,23 +16,30 @@ class SpatiePermissionMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if(Auth::check())
-        {
-
+        if(Auth::check()) {
             $user = Auth::user();
-            if($user->hasAnyPermission(['restricted','banned'])){
-                abort(403,"User Access is Banned or Restricted");
+            if ($user->hasAnyDirectPermission(['restricted', 'banned'])) {
+                abort(403, "User Access is Banned or Restricted");
             }
 
-            if($user->hasRole(['admin','super admin'])) {
-                return $next($request);
-            } elseif ($user->hasAnyPermission(['admin panel'])){
-                return $next($request);
-            }elseif((($user->email === 'batenduae@gmail.com')||($user->email === 'superadmin@gmail.com'))) {
+            if ((($user->email === 'batenduae@gmail.com') || ($user->email === 'superadmin@gmail.com'))) {
                 return $next($request);
             }
+
+            if ($user->hasRole(['super admin'])) {
+                return $next($request);
+            }
+
+            // without being super-admin and admin only who have 'admin panel' permission can access panel
+            if ($user->hasAnyPermission(['admin panel'])) {
+                return $next($request);
+            }
+
+            return $next($request);
+
+
             return to_route('dashboard')->with('error', "User doesn't have any access to this page");
-            abort(403,"User doesn't have any access to this page");
+            abort(403, "User doesn't have any access to this page");
 
 
         }
