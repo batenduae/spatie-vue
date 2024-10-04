12faf782b168c8c5327ad2ece9bd2138f677941d
Baten <batenduae@gmail.com>
2024-10-04 21:16:37 +0600

    latest changes 04-OCt Permission Re-setup

--- resources/js/Pages/Admin/SpatieRolePermission/AssignRolePermission/RolesSyncMassPermission.vue
+++ resources/js/Pages/Admin/SpatieRolePermission/AssignRolePermission/RolesSyncMassPermission.vue
@@ -1,11 +1,11 @@
 <script setup>
 import {defineOptions, onMounted, onUpdated} from "vue";
-import Form from "@/Components/AdminComponents/Forms/Form.vue";
 import AdminButton from "@/Components/AdminComponents/Buttons/AdminButton.vue";
 import Card from "@/Components/AdminComponents/Cards/Card.vue";
 import PageHeader from "@/Components/AdminComponents/Heading/PageHeader.vue";
 import {usePermissions} from "@/composables/permissions.js";
 import SpatieAdminLayout from "@/Layouts/SpatieAdminLayout.vue";
+
 const { hasPermission,showFlash } = usePermissions();
 onMounted(showFlash)
 onUpdated(showFlash)
@@ -30,21 +30,22 @@ defineOptions({ layout: SpatieAdminLayout });
             button-text="Go Back"
             button-type="backward"
             route-name="roles.index"
-            v-if="hasPermission('role.view')"
+            v-if="hasPermission('roles.index')"
         />
     </PageHeader>
 
-    <Card type="cyan" class="mt-4" v-for="role in roles" v-if="hasPermission('assign-permission.to-role')">
+    <Card v-for="role in roles" v-if="hasPermission(['roles.sync.mass-permission','roles.sync.permission'])" class="mt-4"
+          type="cyan">
         <div class="">Roles: {{ role.name }}</div>
         <div class="mx-auto">
             <Card type="green">
-                <Form
-                    :key="role.id"
-                    :permissions="permissions"
-                    :role="role"
-                    route-name="roles.assignPermissionsToRole"
-                >
-                </Form>
+                <!--                <Form-->
+                <!--                    :key="role.id"-->
+                <!--                    :permissions="permissions"-->
+                <!--                    :role="role"-->
+                <!--                    route-name="roles.syncPermission"-->
+                <!--                >-->
+                <!--                </Form>-->
             </Card>
         </div>
     </Card>
