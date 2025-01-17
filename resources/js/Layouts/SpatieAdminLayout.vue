2bf73ff4c51ac56319131a8198ecaa0eee82784f
Baten <batenduae@gmail.com>
2025-01-18 00:58:13 +0600

    latest changes 18/01/2025 Scroll Up Adding

--- resources/js/Layouts/SpatieAdminLayout.vue
+++ resources/js/Layouts/SpatieAdminLayout.vue
@@ -37,9 +37,13 @@ onMounted(showFlash)
 
             <!-- ===== Main Content Start ===== -->
             <main class="w-full flex-1 bgImage dark:bg-boxdark-2 ">
-                <div class="items-center p-4 md:p-6 2xl:p-10 duration-300 ease-linear">
-                    <slot></slot>
-                </div>
+                <ScrollPanel style="height: 93vh">
+                    <div class="items-center p-4 md:p-6 2xl:p-10 duration-300 ease-linear">
+                        <slot></slot>
+                    </div>
+                    <ScrollTop :buttonProps="{ severity: 'danger', raised: true, rounded: true }" :threshold="100" icon="pi pi-arrow-up"
+                               target="parent"/>
+                </ScrollPanel>
             </main>
             <!-- ===== Main Content End ===== -->
         </div>
