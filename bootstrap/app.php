5d2342a4b9781d110e80d68f81ce8e4fb8f4a988
Baten <batenduae@gmail.com>
2025-10-04 22:45:48 +0600

    latest changes 04/10/2025 All Clear

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,8 +6,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use Modules\MultiGuardAuth\app\Http\Middleware\Authenticate;
-use Modules\MultiGuardAuth\app\Http\Middleware\RedirectIfAuthenticated;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
 use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
@@ -26,9 +24,6 @@ return Application::configure(basePath: dirname(__DIR__))
                 'role_or_permission' => RoleOrPermissionMiddleware::class,
                 'isAdmin' =>    SpatiePermissionMiddleware::class,
 
-                //from module MultiGuardAuth
-                'guest' => RedirectIfAuthenticated::class,
-                'auth' => Authenticate::class,
             ])
             ->web(append: [
             HandleInertiaRequests::class,
